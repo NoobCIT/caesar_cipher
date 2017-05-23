c1f99cb0a2b7a87fbe91515f9dc1f5a4d2bb3774
Kevin <nguyenk215@yahoo.com>
2017-05-22 20:29:22 -0700

    Created Caesar Cipher Encrypter

---
+++ caesar_cipher.rb
@@ -0,0 +1,29 @@
+
+
+
+def caesar_cipher(string, shift)
+
+  # Create a-z and z-a array for capital and lowercase letters
+  alphabet_low  = ('a'..'z').to_a + ('a'..'z').to_a
+  alphabet_high = ('A'..'Z').to_a + ('A'..'Z').to_a
+
+  # Append code into array secret
+  secret = []
+
+  # Cycle through each letter
+  array = string.split("")
+  array.each do |letter|
+    if alphabet_high.include?(letter)
+      new_index = alphabet_high.index(letter) + shift
+      secret << alphabet_high[new_index]
+    elsif alphabet_low.include?(letter)
+      new_index = alphabet_low.index(letter) + shift
+      secret << alphabet_low[new_index]
+    else
+      secret << letter
+    end
+  end
+  puts secret.join
+end
+
+caesar_cipher("What a string!", 5)
