196f7ad15c39a4311b332f323cff1c77f294ec4f
Kevin <nguyenk215@yahoo.com>
2017-08-22 20:45:36 -0700

    Fixes error

--- app.rb
+++ app.rb
@@ -2,21 +2,23 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require './lib/caesar_cipher.rb'
 
+
 get '/' do
-  #shift_factor is from index.erb, sets = to shift
-  #phrase is also from index.erb, sets = to string
+  #shift_factor is located in the form for index.erb, sets = to shift
   shift = params['shift_factor'].to_i
+  #phrase is in the form for index.erb, sets = to string
   string = params['phrase']
-  #Then it runs code below
-  encrypted = if shift.nil? && string.nil?
+  encrypted = if shift.nil? || string.nil?
                 "Waiting for Inputs"
               else
                 encrypted_message(string, shift)
               end
-  erb :index, :locals => {:encrypted => encrypted, :shift => shift}
+  erb :index, :locals => {:encrypted => encrypted}
 end
 
 def encrypted_message(string, shift)
   message = caesar_cipher(string, shift)
   "Here's the encryption: #{message}"
 end
+
+#lujanfernaud/sinatra-caesar-cipher
